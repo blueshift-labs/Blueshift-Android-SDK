5be351b658c72bfaa4b473ce0236667303832404
Rahul Raveendran <rahul@bullfin.ch>
2016-09-07 15:24:38 +0530

    updated sdk to support user defined theme for dialog popup.

--- library/src/main/java/com/blueshift/rich_push/NotificationActivity.java
+++ library/src/main/java/com/blueshift/rich_push/NotificationActivity.java
@@ -4,13 +4,21 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.Window;
 
 import com.blueshift.Blueshift;
+import com.blueshift.R;
+import com.blueshift.model.Configuration;
 
+/**
+ * Created by Rahul Raveendran
+ * <p>
+ * This activity is responsible for creating dialog notifications.
+ * Currently supports two types of dialogues.
+ */
 public class NotificationActivity extends AppCompatActivity {
 
     private Context mContext;
@@ -26,9 +34,29 @@ public class NotificationActivity extends AppCompatActivity {
         Message message = (Message) getIntent().getSerializableExtra(RichPushConstants.EXTRA_MESSAGE);
 
         if (message != null) {
-            AlertDialog.Builder builder = new AlertDialog.Builder(mContext);
-            builder.setMessage(message.getBody());
+            int theme = 0;
+
+            /**
+             * Check if there is a user defined theme, if yes assign it to `theme` variable.
+             */
+            Configuration configuration = Blueshift.getInstance(mContext).getConfiguration();
+            if (configuration != null) {
+                theme = configuration.getDialogTheme();
+            }
+
+            AlertDialog.Builder builder;
+
+            /**
+             * check if a valid `theme` is available, else use `BlueshiftDialogTheme`
+             */
+            if (theme == 0) {
+                builder = new AlertDialog.Builder(mContext, R.style.BlueshiftDialogTheme);
+            } else {
+                builder = new AlertDialog.Builder(mContext, theme);
+            }
+
             builder.setTitle(message.getTitle());
+            builder.setMessage(message.getBody());
 
             builder.setOnCancelListener(new DialogInterface.OnCancelListener() {
                 @Override
@@ -52,16 +80,22 @@ public class NotificationActivity extends AppCompatActivity {
         }
     }
 
-
+    /**
+     * This method adds action buttons to the builder based on available category
+     *
+     * @param builder builder on which the atcions to be attached.
+     * @param message the message object that contains the category. this is also used as extra to launching activity
+     * @return updated builder object
+     */
     private AlertDialog.Builder setActions(AlertDialog.Builder builder, final Message message) {
         switch (message.getCategory()) {
             case AlertBoxOpenDismiss:
-                builder.setPositiveButton("Open", new DialogInterface.OnClickListener() {
+                builder.setPositiveButton(R.string.dialog_button_open, new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
 
                         PackageManager packageManager = getPackageManager();
-                        Intent launcherIntent  = packageManager.getLaunchIntentForPackage(getPackageName());
+                        Intent launcherIntent = packageManager.getLaunchIntentForPackage(getPackageName());
                         launcherIntent.putExtra(RichPushConstants.EXTRA_MESSAGE, message);
                         launcherIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                         startActivity(launcherIntent);
@@ -72,12 +106,12 @@ public class NotificationActivity extends AppCompatActivity {
                     }
                 });
 
-                builder.setNegativeButton("Dismiss", null);
+                builder.setNegativeButton(R.string.dialog_button_dismiss, null);
 
                 break;
 
             case AlertBoxDismiss:
-                builder.setNegativeButton("Dismiss", null);
+                builder.setNegativeButton(R.string.dialog_button_dismiss, null);
 
                 break;
         }
