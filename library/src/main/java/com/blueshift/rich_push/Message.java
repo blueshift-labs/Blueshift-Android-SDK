759a643a90ee7563d6effa6ac8a276ab8007e419
Rahul Raveendran <rahul@bullfin.ch>
2016-09-20 11:56:27 +0530

    cleaned up the code, added some javadoc

--- library/src/main/java/com/blueshift/rich_push/Message.java
+++ library/src/main/java/com/blueshift/rich_push/Message.java
@@ -13,12 +13,18 @@ public class Message implements Serializable {
     public static final String EXTRA_BSFT_EXPERIMENT_UUID = "bsft_experiment_uuid";
     public static final String EXTRA_BSFT_USER_UUID = "bsft_user_uuid";
 
-    // these parameters comes outside 'message' in push message.
-    // we're adding them inside for avoiding major code changes in sdk.
+    /**
+     * Following are the campaign uuids. They come outside the 'message' object in push message.
+     * We have added them inside this class to avoid major code change and to use them conveniently
+     * as the message object is passed along with all notification methods generally.
+     */
     private String bsft_experiment_uuid;
     private String bsft_user_uuid;
 
-    // Message payload values
+    /**
+     * The following variables are used for parsing the 'message' payload.
+     * They are the values used for creating the notification.
+     */
     private String id;
     private String notification_type;
     private String title;
@@ -32,6 +38,19 @@ public class Message implements Serializable {
     private long expiry;
     private HashMap<String, Object> data;
     private CarouselElement[] carousel_elements;
+
+    /**
+     * Following are the variables used for managing the logics implemented locally.
+     *
+     * Ex: the index of carousel notification item, this will keep track of currently shown image in
+     * a carousel type notification. helps the sdk to implement actions of 'next' & 'prev' buttons
+     */
+    private int carousel_current_index = 0;
+
+
+    /**
+     * The following are the get / set methods for the above declared variables.
+     */
 
     public String getBsftExperimentUuid() {
         return bsft_experiment_uuid;
@@ -49,10 +68,11 @@ public class Message implements Serializable {
         this.bsft_user_uuid = bsftUserUuid;
     }
 
-    public boolean isCampaignPush() {
-        return !TextUtils.isEmpty(bsft_experiment_uuid) && !TextUtils.isEmpty(bsft_user_uuid);
-    }
-
+    /**
+     * This method generates the HashMap with experiment uuid and user uuid filled in (if available)
+     *
+     * @return valid parameters inside HashMap if it is a campaign push, else null
+     */
     public HashMap<String, Object> getCampaignAttr() {
         HashMap<String, Object> attributes = null;
 
@@ -113,11 +133,19 @@ public class Message implements Serializable {
         return data;
     }
 
-    public boolean isSilentPush() {
-        return getNotificationType() == NotificationType.Notification && getCategory() == NotificationCategory.SilentPush;
-    }
-
     public CarouselElement[] getCarouselElements() {
         return carousel_elements;
+    }
+
+    /**
+     * Following are the methods that helps the sdk to understand the type of the notification.
+     */
+
+    public boolean isCampaignPush() {
+        return !TextUtils.isEmpty(bsft_experiment_uuid) && !TextUtils.isEmpty(bsft_user_uuid);
+    }
+
+    public boolean isSilentPush() {
+        return getNotificationType() == NotificationType.Notification && getCategory() == NotificationCategory.SilentPush;
     }
 }
