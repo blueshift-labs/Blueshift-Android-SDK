4b14bcd739d4bbc503cafe5eb1673c76f06a9fef
Rahul Raveendran <rahul@bullfin.ch>
2016-10-21 11:04:51 +0530

    added more javadoc to carousel element class

--- android-sdk/src/main/java/com/blueshift/rich_push/CarouselElement.java
+++ android-sdk/src/main/java/com/blueshift/rich_push/CarouselElement.java
@@ -6,14 +6,40 @@ import java.io.Serializable;
 import java.util.HashMap;
 
 /**
+ * This model represents the carousel element in carousel type notifications.
+ *
  * @author Rahul Raveendran V P
  *         Created on 16/9/16 @ 3:03 PM
  *         https://github.com/rahulrvp
  */
 public class CarouselElement implements Serializable {
+
+    /**
+     * The image URL used for rendering image in Notification
+     */
     private String image_url;
+
+    /**
+     * Optional action to perform when a carousel image is clicked. Supported values are,
+     * - ACTION_OPEN_APP (Default)
+     * - ACTION_VIEW
+     * - ACTION_BUY
+     * - ACTION_OPEN_CART
+     * - ACTION_OPEN_OFFER_PAGE
+     */
     private String action;
+
+    /**
+     * On clicking notification, app will be opened and this URL will be passed along with the
+     * corresponding carousel element object to the launching activity of the app.
+     * <p>
+     * > Note: If this key is present, it will override the above actions.
+     */
     private String deep_link_url;
+
+    /**
+     * Optional additional parameters the developer can send.
+     */
     private HashMap data;
 
     public String getImageUrl() {
