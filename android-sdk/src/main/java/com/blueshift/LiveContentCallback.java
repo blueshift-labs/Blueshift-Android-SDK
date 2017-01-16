6bce4bcd8f71d9435f4f4ce330717c4b3235935a
Rahul Raveendran <rahul@bullfin.ch>
2017-01-16 11:40:56 +0530

    javadoc for interface

--- android-sdk/src/main/java/com/blueshift/LiveContentCallback.java
+++ android-sdk/src/main/java/com/blueshift/LiveContentCallback.java
@@ -1,6 +1,10 @@
 package com.blueshift;
 
 /**
+ * This interface provides a callback method 'onReceive()' which is
+ * used by the live content API inside Blueshift sdk. It is responsible
+ * for delivering the api response received from the server to the caller.
+ *
  * @author Rahul Raveendran V P
  *         Created on 13/1/17 @ 3:16 PM
  *         https://github.com/rahulrvp
