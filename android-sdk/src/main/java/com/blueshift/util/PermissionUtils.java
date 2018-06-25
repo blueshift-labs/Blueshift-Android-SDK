2111de50c2e595b0baff22d6d56a271bd6f43328
Rahul Raveendran <rahul.pmna@gmail.com>
2018-06-25 16:35:27 +0530

    enabled proper null checks inside permission utils

--- android-sdk/src/main/java/com/blueshift/util/PermissionUtils.java
+++ android-sdk/src/main/java/com/blueshift/util/PermissionUtils.java
@@ -3,15 +3,18 @@ package com.blueshift.util;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.support.v4.content.ContextCompat;
+import android.text.TextUtils;
 import android.util.Log;
 
 public class PermissionUtils {
     private static final String LOG_TAG = "Blueshift";
 
     public static boolean hasAnyPermission(Context context, String[] permissions) {
-        for (String permission : permissions) {
-            if (hasPermission(context, permission)) {
-                return true;
+        if (context != null && permissions != null) {
+            for (String permission : permissions) {
+                if (hasPermission(context, permission)) {
+                    return true;
+                }
             }
         }
 
@@ -19,9 +22,11 @@ public class PermissionUtils {
     }
 
     public static boolean hasAllPermissions(Context context, String[] permissions) {
-        for (String permission : permissions) {
-            if (!hasPermission(context, permission)) {
-                return false;
+        if (context != null && permissions != null) {
+            for (String permission : permissions) {
+                if (!hasPermission(context, permission)) {
+                    return false;
+                }
             }
         }
 
@@ -31,9 +36,11 @@ public class PermissionUtils {
     public static boolean hasPermission(Context context, String permission) {
         boolean hasPermission = false;
         try {
-            hasPermission =
-                    ContextCompat.checkSelfPermission(context, permission)
-                            == PackageManager.PERMISSION_GRANTED;
+            if (context != null && !TextUtils.isEmpty(permission)) {
+                hasPermission =
+                        ContextCompat.checkSelfPermission(context, permission)
+                                == PackageManager.PERMISSION_GRANTED;
+            }
         } catch (Exception e) {
             Log.e(LOG_TAG, e.getMessage());
         }
