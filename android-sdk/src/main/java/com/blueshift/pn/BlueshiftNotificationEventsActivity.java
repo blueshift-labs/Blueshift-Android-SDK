aec27db65dea6cf4ecf0ec0091ba09ebf341fe38
Rahul Raveendran <rahul.raveendran@getblueshift.com>
2020-06-18 19:28:36 +0530

    checks for null intent and assigns app open intent to the same

--- android-sdk/src/main/java/com/blueshift/pn/BlueshiftNotificationEventsActivity.java
+++ android-sdk/src/main/java/com/blueshift/pn/BlueshiftNotificationEventsActivity.java
@@ -49,21 +49,24 @@ public class BlueshiftNotificationEventsActivity extends AppCompatActivity {
                     // mark 'click'
                     Blueshift.getInstance(this).trackNotificationClick(message);
 
-                    Intent intent;
+                    Intent intent = null;
 
-                    if (TextUtils.isEmpty(action)) {
-                        // open app if no action is provided.
-                        intent = NotificationUtils.getOpenAppIntent(this, message);
-                    } else if (action.equals(RichPushConstants.ACTION_VIEW(this))) {
-                        intent = NotificationUtils.getViewProductActivityIntent(this, message);
-                    } else if (action.equals(RichPushConstants.ACTION_BUY(this))) {
-                        intent = NotificationUtils.getAddToCartActivityIntent(this, message);
-                    } else if (action.equals(RichPushConstants.ACTION_OPEN_CART(this))) {
-                        intent = NotificationUtils.getViewCartActivityIntent(this, message);
-                    } else if (action.equals(RichPushConstants.ACTION_OPEN_OFFER_PAGE(this))) {
-                        intent = NotificationUtils.getViewOffersActivityIntent(this, message);
-                    } else {
-                        // for ACTION OPEN and other unknown actions, open the app.
+                    if (!TextUtils.isEmpty(action)) {
+                        if (action.equals(RichPushConstants.ACTION_OPEN_APP(this))) {
+                            intent = NotificationUtils.getOpenAppIntent(this, message);
+                        } else if (action.equals(RichPushConstants.ACTION_VIEW(this))) {
+                            intent = NotificationUtils.getViewProductActivityIntent(this, message);
+                        } else if (action.equals(RichPushConstants.ACTION_BUY(this))) {
+                            intent = NotificationUtils.getAddToCartActivityIntent(this, message);
+                        } else if (action.equals(RichPushConstants.ACTION_OPEN_CART(this))) {
+                            intent = NotificationUtils.getViewCartActivityIntent(this, message);
+                        } else if (action.equals(RichPushConstants.ACTION_OPEN_OFFER_PAGE(this))) {
+                            intent = NotificationUtils.getViewOffersActivityIntent(this, message);
+                        }
+                    }
+
+                    if (intent == null) {
+                        // make sure the app is opened even if no category deep-links are available
                         intent = NotificationUtils.getOpenAppIntent(this, message);
                     }
 
