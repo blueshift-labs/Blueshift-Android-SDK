eb45658a52e27cd2fcd489a99d1b10e96a2aa2ed
Rahul Raveendran <rahul.raveendran@getblueshift.com>
2020-08-30 16:48:14 +0530

    moved job task to a new thread instead of pushing to executor

--- android-sdk/src/main/java/com/blueshift/batch/BulkEventJobService.java
+++ android-sdk/src/main/java/com/blueshift/batch/BulkEventJobService.java
@@ -2,40 +2,45 @@ package com.blueshift.batch;
 
 import android.app.job.JobParameters;
 import android.app.job.JobService;
-import android.content.Context;
 import android.os.Build;
 import android.support.annotation.RequiresApi;
 
-import com.blueshift.BlueshiftExecutor;
 import com.blueshift.BlueshiftLogger;
 
 @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
 public class BulkEventJobService extends JobService {
-
-    private static final String LOG_TAG = BulkEventJobService.class.getSimpleName();
+    private static final String TAG = "BulkEventJobService";
 
     @Override
-    public boolean onStartJob(final JobParameters params) {
-        final Context context = this;
+    public boolean onStartJob(JobParameters jobParameters) {
+        BlueshiftLogger.d(TAG, "Job started.");
+        doBackgroundWork(jobParameters);
 
-        BlueshiftExecutor.getInstance().runOnDiskIOThread(
-                new Runnable() {
-                    @Override
-                    public void run() {
-                        BlueshiftLogger.d(LOG_TAG, "Enqueue bulk events started.");
-                        BulkEventManager.enqueueBulkEvents(context);
-                        BlueshiftLogger.d(LOG_TAG, "Enqueue bulk events completed.");
+        // The job should continue running as the enqueue operation may take a while
+        // we have the jobFinished method called once the task is complete.
+        return true;
+    }
 
-                        jobFinished(params, true);
-                    }
-                }
-        );
+    private void doBackgroundWork(final JobParameters jobParameters) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                BulkEventManager.enqueueBulkEvents(getApplicationContext());
 
-        return false;
+                // this is a periodic job, we don't need the job scheduler to
+                // reschedule this with available backoff policy. hence passing
+                // false as 2nd argument.
+                jobFinished(jobParameters, false);
+
+                BlueshiftLogger.d(TAG, "Job completed.");
+            }
+        }).start();
     }
 
     @Override
     public boolean onStopJob(JobParameters jobParameters) {
+        BlueshiftLogger.d(TAG, "Job cancel requested.");
+
         return false;
     }
 }
--- android-sdk/src/main/java/com/blueshift/httpmanager/request_queue/RequestQueueJobService.java
+++ android-sdk/src/main/java/com/blueshift/httpmanager/request_queue/RequestQueueJobService.java
@@ -5,6 +5,7 @@ import android.app.job.JobService;
 import android.os.Build;
 import android.support.annotation.RequiresApi;
 
+import com.blueshift.BlueshiftLogger;
 import com.blueshift.request_queue.RequestQueue;
 
 
@@ -17,18 +18,39 @@ import com.blueshift.request_queue.RequestQueue;
 
 @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
 public class RequestQueueJobService extends JobService {
+    private static final String TAG = "RequestQueueJobService";
 
     @Override
     public boolean onStartJob(JobParameters jobParameters) {
-        RequestQueue.getInstance().syncInBackground(this);
-        // this method needs to be called to release the wakelock
-        jobFinished(jobParameters, false);
+        BlueshiftLogger.d(TAG, "Job started.");
+        doBackgroundWork(jobParameters);
 
+        // The job should continue running as the enqueue operation may take a while
+        // we have the jobFinished method called once the task is complete.
         return true;
     }
 
+    private void doBackgroundWork(final JobParameters jobParameters) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                RequestQueue.getInstance().sync(getApplicationContext());
+
+                // this is a periodic job, we don't need the job scheduler to
+                // reschedule this with available backoff policy. hence passing
+                // false as 2nd argument.
+                jobFinished(jobParameters, false);
+
+                BlueshiftLogger.d(TAG, "Job completed.");
+            }
+        }).start();
+    }
+
+
     @Override
     public boolean onStopJob(JobParameters jobParameters) {
+        BlueshiftLogger.d(TAG, "Job cancel requested.");
+
         return false;
     }
 
