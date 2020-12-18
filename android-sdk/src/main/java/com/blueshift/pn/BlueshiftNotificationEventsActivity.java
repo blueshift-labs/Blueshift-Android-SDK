1365629b9e09cff80afae5f69c48de585edb38c4
Rahul Raveendran <rahul.raveendran@getblueshift.com>
2020-12-18 11:30:06 +0530

    added helper method in util class to handle push msg clicks

--- android-sdk/src/main/java/com/blueshift/pn/BlueshiftNotificationEventsActivity.java
+++ android-sdk/src/main/java/com/blueshift/pn/BlueshiftNotificationEventsActivity.java
@@ -27,8 +27,6 @@ import java.util.HashMap;
 
 public class BlueshiftNotificationEventsActivity extends AppCompatActivity {
 
-    private static final String LOG_TAG = "NotificationClick";
-
     @Override
     protected void onResume() {
         super.onResume();
@@ -40,70 +38,6 @@ public class BlueshiftNotificationEventsActivity extends AppCompatActivity {
     }
 
     protected void processAction(String action, Bundle extraBundle) {
-        if (extraBundle != null) {
-            Message message = (Message) extraBundle.getSerializable(RichPushConstants.EXTRA_MESSAGE);
-            if (message != null) {
-                try {
-                    HashMap<String, Object> clickAttr = new HashMap<>();
-                    String deepLink = extraBundle.getString(RichPushConstants.EXTRA_DEEP_LINK_URL);
-                    clickAttr.put(BlueshiftConstants.KEY_CLICK_URL, deepLink);
-
-                    // mark 'click'
-                    Blueshift.getInstance(this).trackNotificationClick(message, clickAttr);
-
-                    Intent intent = null;
-
-                    if (!TextUtils.isEmpty(action)) {
-                        if (action.equals(RichPushConstants.ACTION_OPEN_APP(this))) {
-                            intent = NotificationUtils.getOpenAppIntent(this, message);
-                        } else if (action.equals(RichPushConstants.ACTION_VIEW(this))) {
-                            intent = NotificationUtils.getViewProductActivityIntent(this, message);
-                        } else if (action.equals(RichPushConstants.ACTION_BUY(this))) {
-                            intent = NotificationUtils.getAddToCartActivityIntent(this, message);
-                        } else if (action.equals(RichPushConstants.ACTION_OPEN_CART(this))) {
-                            intent = NotificationUtils.getViewCartActivityIntent(this, message);
-                        } else if (action.equals(RichPushConstants.ACTION_OPEN_OFFER_PAGE(this))) {
-                            intent = NotificationUtils.getViewOffersActivityIntent(this, message);
-                        }
-                    }
-
-                    if (intent == null) {
-                        // make sure the app is opened even if no category deep-links are available
-                        intent = NotificationUtils.getOpenAppIntent(this, message);
-                    }
-
-                    // add complete bundle to the intent.
-                    intent.putExtras(extraBundle);
-
-                    // Note: This will create a new task and launch the app with the corresponding
-                    // activity. As per the docs, the dev should add parent activity to all the
-                    // activities registered in the manifest in order to get the back stack working
-                    // doc: https://developer.android.com/training/notify-user/navigation#DirectEntry
-                    TaskStackBuilder.create(this).addNextIntentWithParentStack(intent).startActivities();
-
-                    // mark 'app_open'
-                    Blueshift.getInstance(this).trackNotificationPageOpen(message, false);
-
-                    // remove cached images(if any) for this notification
-                    NotificationUtils.removeCachedCarouselImages(this, message);
-
-                    // remove notification from tray
-                    NotificationManager notificationManager =
-                            (NotificationManager) this.getSystemService(Context.NOTIFICATION_SERVICE);
-                    if (notificationManager != null) {
-                        int notificationID = intent.getIntExtra(RichPushConstants.EXTRA_NOTIFICATION_ID, 0);
-                        notificationManager.cancel(notificationID);
-                    }
-
-                    sendBroadcast(new Intent(Intent.ACTION_CLOSE_SYSTEM_DIALOGS));
-                } catch (Exception e) {
-                    BlueshiftLogger.e(LOG_TAG, e);
-                }
-            } else {
-                BlueshiftLogger.d(LOG_TAG, "No message found inside bundle.");
-            }
-        } else {
-            BlueshiftLogger.d(LOG_TAG, "No bundle found from the notification click event.");
-        }
+        NotificationUtils.processNotificationClick(this, action, extraBundle);
     }
 }
