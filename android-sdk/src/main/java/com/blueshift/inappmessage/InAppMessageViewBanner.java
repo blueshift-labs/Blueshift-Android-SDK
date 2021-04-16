fa23afa5c48f625631fee2309a6274a70c0d8204
Rahul Raveendran <rahul.raveendran@getblueshift.com>
2021-04-17 00:06:09 +0530

    committing working swipe code. need to tackle click

--- android-sdk/src/main/java/com/blueshift/inappmessage/InAppMessageViewBanner.java
+++ android-sdk/src/main/java/com/blueshift/inappmessage/InAppMessageViewBanner.java
@@ -1,12 +1,17 @@
 package com.blueshift.inappmessage;
 
 import android.content.Context;
+import android.os.Build;
+import android.util.Log;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.blueshift.BlueshiftLogger;
+import com.blueshift.R;
 import com.blueshift.util.CommonUtils;
 
 import org.json.JSONArray;
@@ -19,14 +24,15 @@ import static android.view.ViewGroup.LayoutParams.WRAP_CONTENT;
 
 public class InAppMessageViewBanner extends InAppMessageView {
     private static final String TAG = InAppMessageViewBanner.class.getSimpleName();
+    private OnClickListener onClickListener = null;
 
     public InAppMessageViewBanner(Context context, InAppMessage inAppMessage) {
         super(context, inAppMessage);
     }
 
     @Override
-    public View getView(InAppMessage inAppMessage) {
-        LinearLayout linearLayout = new LinearLayout(getContext());
+    public View getView(final InAppMessage inAppMessage) {
+        final InAppSwipeLinearLayout linearLayout = new InAppSwipeLinearLayout(getContext());
         linearLayout.setGravity(CENTER);
         linearLayout.setBackgroundResource(android.R.color.transparent);
 
@@ -60,7 +66,8 @@ public class InAppMessageViewBanner extends InAppMessageView {
         if (actions != null && actions.length() > 0) {
             try {
                 JSONObject actionJson = actions.getJSONObject(0);
-                linearLayout.setOnClickListener(getActionClickListener(actionJson, null));
+                onClickListener = getActionClickListener(actionJson, null);
+                if (onClickListener == null) Log.d(TAG, "getView: null listener");
             } catch (JSONException e) {
                 BlueshiftLogger.e(TAG, e);
             }
@@ -68,6 +75,81 @@ public class InAppMessageViewBanner extends InAppMessageView {
 
         linearLayout.setLayoutParams(new LinearLayout.LayoutParams(MATCH_PARENT, WRAP_CONTENT));
         linearLayout.setMinimumHeight(dp48);
+
+        linearLayout.enableSwipeAndTap(
+                new InAppSwipeLinearLayout.OnSwipeGestureListener() {
+                    @Override
+                    public void onSwipeUp() {
+                        Log.d(TAG, "onSwipeUp: ");
+                    }
+
+                    @Override
+                    public void onSwipeDown() {
+                        Log.d(TAG, "onSwipeDown: ");
+                    }
+
+                    @Override
+                    public void onSwipeLeft() {
+                        Log.d(TAG, "onSwipeLeft: ");
+
+                        Animation animation = AnimationUtils.loadAnimation(getContext(), R.anim.bsft_translate_left_side);
+                        linearLayout.setAnimation(animation);
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+                            linearLayout
+                                    .animate()
+                                    .translationX(-linearLayout.getWidth())
+                                    .setDuration(1000)
+                                    .withEndAction(new Runnable() {
+                                        @Override
+                                        public void run() {
+                                            onDismiss(inAppMessage, null);
+                                        }
+                                    }).start();
+                        }
+                    }
+
+                    @Override
+                    public void onSwipeRight() {
+                        Log.d(TAG, "onSwipeRight: ");
+                        Animation animation = AnimationUtils.loadAnimation(getContext(), R.anim.bsft_translate_right_side);
+                        linearLayout.setAnimation(animation);
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+                            linearLayout
+                                    .animate()
+                                    .translationX(linearLayout.getWidth())
+                                    .setDuration(1000)
+                                    .withEndAction(new Runnable() {
+                                        @Override
+                                        public void run() {
+                                            onDismiss(inAppMessage, null);
+                                        }
+                                    }).start();
+                        }
+
+                    }
+
+                    @Override
+                    public void onSingleTapConfirmed() {
+                        Log.d(TAG, "onSingleTapConfirmed: 2");
+                        onClickListener.onClick(linearLayout);
+
+                        Animation animation = AnimationUtils.loadAnimation(getContext(), R.anim.bsft_translate_right_side);
+                        linearLayout.setAnimation(animation);
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+                            linearLayout
+                                    .animate()
+                                    .translationX(linearLayout.getWidth())
+                                    .setDuration(1000)
+                                    .withEndAction(new Runnable() {
+                                        @Override
+                                        public void run() {
+//                                            onClickListener.onClick(linearLayout);
+                                        }
+                                    }).start();
+                        }
+
+                    }
+                });
 
         return linearLayout;
     }
---
+++ android-sdk/src/main/java/com/blueshift/inappmessage/InAppSwipeLinearLayout.java
@@ -0,0 +1,110 @@
+package com.blueshift.inappmessage;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.LinearLayout;
+
+public class InAppSwipeLinearLayout extends LinearLayout {
+
+    private static final String TAG = "InAppSwipeLinearLayout";
+
+    public InAppSwipeLinearLayout(Context context) {
+        super(context);
+    }
+
+    public InAppSwipeLinearLayout(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public InAppSwipeLinearLayout(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    public boolean performClick() {
+        return super.performClick();
+    }
+
+    public void enableSwipeAndTap(OnSwipeGestureListener gestureListener) {
+
+        final GestureDetector gestureDetector = new GestureDetector(
+                getContext(),
+                new SwipeGestureListener(gestureListener));
+
+        this.setOnTouchListener(
+                new OnTouchListener() {
+                    @SuppressLint("ClickableViewAccessibility")
+                    @Override
+                    public boolean onTouch(View view, MotionEvent motionEvent) {
+                        gestureDetector.onTouchEvent(motionEvent);
+                        return true;
+                    }
+                });
+    }
+
+    static class SwipeGestureListener extends GestureDetector.SimpleOnGestureListener {
+        private static final int SWIPE_MIN_DISTANCE = 120;
+        private static final int SWIPE_THRESHOLD_VELOCITY = 200;
+
+        private final OnSwipeGestureListener gestureListener;
+
+        public SwipeGestureListener(OnSwipeGestureListener gestureListener) {
+            this.gestureListener = gestureListener;
+        }
+
+        @Override
+        public boolean onSingleTapConfirmed(MotionEvent e) {
+            Log.d(TAG, "onSingleTapConfirmed: ");
+
+            if (gestureListener != null) {
+                gestureListener.onSingleTapConfirmed();
+                return true;
+            }
+
+            return super.onSingleTapConfirmed(e);
+        }
+
+        @Override
+        public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+            Log.d(TAG, "onFling: ");
+
+            if (e1.getX() - e2.getX() > SWIPE_MIN_DISTANCE
+                    && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY) {
+                // swipe right to left
+                if (gestureListener != null) gestureListener.onSwipeLeft();
+            } else if (e2.getX() - e1.getX() > SWIPE_MIN_DISTANCE
+                    && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY) {
+                // swipe left to right
+                if (gestureListener != null) gestureListener.onSwipeRight();
+            } else if (e1.getY() - e2.getY() > SWIPE_MIN_DISTANCE
+                    && Math.abs(velocityY) > SWIPE_THRESHOLD_VELOCITY) {
+                // top to bottom
+                if (gestureListener != null) gestureListener.onSwipeDown();
+            } else if (e2.getY() - e1.getY() > SWIPE_MIN_DISTANCE
+                    && Math.abs(velocityY) > SWIPE_THRESHOLD_VELOCITY) {
+                // bottom to top
+                if (gestureListener != null) gestureListener.onSwipeUp();
+            }
+
+            return super.onFling(e1, e2, velocityX, velocityY);
+        }
+    }
+
+    public interface OnSwipeGestureListener {
+        void onSwipeUp();
+
+        void onSwipeDown();
+
+        void onSwipeLeft();
+
+        void onSwipeRight();
+
+        void onSingleTapConfirmed();
+    }
+}
