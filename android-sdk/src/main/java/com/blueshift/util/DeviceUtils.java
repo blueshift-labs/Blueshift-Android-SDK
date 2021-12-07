f07fa34efc82af9f7e0106731aa7acf4e15b615a
Rahul Raveendran V P <rahul.raveendran@getblueshift.com>
2021-12-07 22:16:47 +0530

    [MOBL-800] Firebase Messaging SDK update (#292)
    * adjusted code to use new api methods to get id and token. fixed the sdk versions to minimum.
    
    * replaced id with token
    
    * added try catch blocks to avoid crashes
    
    * changed firebase messaging to min version that has the required api

--- android-sdk/src/main/java/com/blueshift/util/DeviceUtils.java
+++ android-sdk/src/main/java/com/blueshift/util/DeviceUtils.java
@@ -4,20 +4,12 @@ import android.content.ActivityNotFoundException;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
-import androidx.annotation.WorkerThread;
 import android.telephony.TelephonyManager;
-import android.text.TextUtils;
 
-import com.blueshift.BlueShiftPreference;
+import com.blueshift.BlueshiftAttributesApp;
 import com.blueshift.BlueshiftLogger;
-import com.blueshift.R;
-import com.blueshift.model.Configuration;
 import com.google.android.gms.ads.identifier.AdvertisingIdClient;
-import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
-import com.google.android.gms.common.GooglePlayServicesRepairableException;
-import com.google.firebase.iid.FirebaseInstanceId;
 
-import java.io.IOException;
 import java.net.Inet4Address;
 import java.net.InetAddress;
 import java.net.NetworkInterface;
@@ -26,8 +18,8 @@ import java.util.Enumeration;
 
 /**
  * @author Rahul Raveendran V P
- *         Created on 5/3/15 @ 2:00 PM
- *         https://github.com/rahulrvp
+ * Created on 5/3/15 @ 2:00 PM
+ * https://github.com/rahulrvp
  */
 public class DeviceUtils {
     private static final String LOG_TAG = DeviceUtils.class.getSimpleName();
@@ -52,63 +44,13 @@ public class DeviceUtils {
     }
 
     /**
-     * This method is responsible for returning a valid device_id string.
+     * Returns the cached device_id based on the device_id source specified during initialization.
      *
-     * Depending upon if the developer has enabled collection of Advertising Id
-     * as device_id, it will return advertising id or GUID.
-     *
-     * As the Advertising Id should be collected from worker thread, this method
-     * should be called from worker thread only.
-     *
-     * @param context Valid {@link Context} object
-     * @return Valid device_id string
+     * @param context Nullable context variable.
+     * @return Valid device_id if available at the time of call. Else, returns null.
      */
-    @WorkerThread
     public static String getDeviceId(Context context) {
-        String deviceId = null;
-
-        Configuration configuration = BlueshiftUtils.getConfiguration(context);
-        if (configuration != null && configuration.getDeviceIdSource() != null) {
-            switch (configuration.getDeviceIdSource()) {
-                case INSTANCE_ID:
-                    deviceId = FirebaseInstanceId.getInstance().getId();
-                    break;
-                case GUID:
-                    deviceId = BlueShiftPreference.getDeviceID(context);
-                    break;
-                case ADVERTISING_ID_PKG_NAME:
-                    try {
-                        deviceId = getAdvertisingId(context);
-                        deviceId += (":" + context.getPackageName());
-                    } catch (Exception e) {
-                        BlueshiftLogger.e(LOG_TAG, "Could not build \"advertising id - pkg name\" combo.");
-                        BlueshiftLogger.e(LOG_TAG, e);
-                    }
-                    break;
-                case INSTANCE_ID_PKG_NAME:
-                    try {
-                        deviceId = FirebaseInstanceId.getInstance().getId();
-                        deviceId += (":" + context.getPackageName());
-                    } catch (Exception e) {
-                        BlueshiftLogger.e(LOG_TAG, "Could not build \"instance id - pkg name\" combo.");
-                        BlueshiftLogger.e(LOG_TAG, e);
-                    }
-                    break;
-                case CUSTOM:
-                    deviceId = configuration.getCustomDeviceId();
-                    if (TextUtils.isEmpty(deviceId)) {
-                        BlueshiftLogger.e(LOG_TAG, "Custom device id is not provided!");
-                    }
-                    break;
-                default:
-                    // ADVERTISING_ID & Others
-                    deviceId = getAdvertisingId(context);
-            }
-        } else {
-            deviceId = getAdvertisingId(context);
-        }
-
-        return deviceId;
+        return BlueshiftAttributesApp.getInstance().getCachedDeviceId();
     }
 
     public static String getAdvertisingId(Context context) {
