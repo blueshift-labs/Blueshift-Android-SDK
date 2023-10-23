8874f760b733958ddd70e83dcd06e2507cbf46ff
Rahul Raveendran V P <rahul.raveendran@getblueshift.com>
2023-10-23 15:12:22 +0530

    [MOBL-1408] Added country code and language code to the event attributes (#343)

--- android-sdk/src/main/java/com/blueshift/BlueshiftAttributesApp.java
+++ android-sdk/src/main/java/com/blueshift/BlueshiftAttributesApp.java
@@ -26,6 +26,8 @@ import com.google.firebase.messaging.FirebaseMessaging;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.Locale;
+
 public class BlueshiftAttributesApp extends JSONObject {
     private static final String TAG = "ApplicationAttributes";
     private static final BlueshiftAttributesApp instance = new BlueshiftAttributesApp();
@@ -104,8 +106,44 @@ public class BlueshiftAttributesApp extends JSONObject {
         addDeviceAdId(context);
         addDeviceLocation(context);
         addAdTrackingStatus(context);
+        addDeviceCountryCode();
+        addDeviceLanguageCode();
 
         runPermissionChecksAndLogAsync(context);
+    }
+
+    private void addDeviceCountryCode() {
+        String countryCode = Locale.getDefault().getCountry();
+        setDeviceCountryCode(countryCode);
+    }
+
+    private void setDeviceCountryCode(String countryCode) {
+        synchronized (instance) {
+            if (countryCode != null) {
+                try {
+                    instance.put(BlueshiftConstants.KEY_COUNTRY_CODE, countryCode);
+                } catch (JSONException e) {
+                    BlueshiftLogger.e(TAG, e);
+                }
+            }
+        }
+    }
+
+    private void addDeviceLanguageCode() {
+        String languageCode = Locale.getDefault().getLanguage();
+        setDeviceLanguageCode(languageCode);
+    }
+
+    private void setDeviceLanguageCode(String languageCode) {
+        synchronized (instance) {
+            if (languageCode != null) {
+                try {
+                    instance.put(BlueshiftConstants.KEY_LANGUAGE_CODE, languageCode);
+                } catch (JSONException e) {
+                    BlueshiftLogger.e(TAG, e);
+                }
+            }
+        }
     }
 
     private void addDeviceAdId(final Context context) {
@@ -537,6 +575,18 @@ public class BlueshiftAttributesApp extends JSONObject {
      */
     @WorkerThread
     public BlueshiftAttributesApp sync(Context context) {
+        try {
+            addDeviceCountryCode();
+        } catch (Exception e) {
+            BlueshiftLogger.e(TAG, e);
+        }
+
+        try {
+            addDeviceLanguageCode();
+        } catch (Exception e) {
+            BlueshiftLogger.e(TAG, e);
+        }
+
         try {
             addDeviceLocation(context);
         } catch (Exception e) {
