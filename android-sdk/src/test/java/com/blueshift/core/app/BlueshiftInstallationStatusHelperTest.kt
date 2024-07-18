edcf9031a297a4873b51e5cf9abd13d4922f9e0d
Rahul Raveendran <rahul.raveendran@getblueshift.com>
2024-07-18 14:47:36 +0530

    Added more test cases to cover getEventAttributes method

--- android-sdk/src/test/java/com/blueshift/core/app/BlueshiftInstallationStatusHelperTest.kt
+++ android-sdk/src/test/java/com/blueshift/core/app/BlueshiftInstallationStatusHelperTest.kt
@@ -1,6 +1,7 @@
 package com.blueshift.core.app
 
 import android.util.Log
+import com.blueshift.BlueshiftConstants
 import io.mockk.every
 import io.mockk.mockk
 import io.mockk.mockkStatic
@@ -53,5 +54,35 @@ class BlueshiftInstallationStatusHelperTest {
         val database = mockk<File>()
         val status = helper.getInstallationStatus("1.0.0", "1.0.0", database)
         assert(status == BlueshiftInstallationStatus.NO_CHANGE)
+    }
+
+    @Test
+    fun getEventAttributes_returnHashMapWithAppInstalledAtWhenStatusIsAppInstall() {
+        val helper = BlueshiftInstallationStatusHelper()
+        val attributes = helper.getEventAttributes(BlueshiftInstallationStatus.APP_INSTALL, null)
+        assert(attributes[BlueshiftConstants.KEY_APP_INSTALLED_AT] != null)
+    }
+
+    @Test
+    fun getEventAttributes_returnHashMapWithAppUpdatedAtAndPreviousAppVersionWhenStatusIsAppUpdate() {
+        val helper = BlueshiftInstallationStatusHelper()
+        val attributes = helper.getEventAttributes(BlueshiftInstallationStatus.APP_UPDATE, "1.0.0")
+        assert(attributes[BlueshiftConstants.KEY_APP_UPDATED_AT] != null)
+        assert(attributes[BlueshiftConstants.KEY_PREVIOUS_APP_VERSION] == "1.0.0")
+    }
+
+    @Test
+    fun getEventAttributes_returnHashMapWithAppUpdatedAtWhenStatusIsAppUpdateAndPreviousAppVersionIsNull() {
+        val helper = BlueshiftInstallationStatusHelper()
+        val attributes = helper.getEventAttributes(BlueshiftInstallationStatus.APP_UPDATE, null)
+        assert(attributes[BlueshiftConstants.KEY_APP_UPDATED_AT] != null)
+        assert(!attributes.contains(BlueshiftConstants.KEY_PREVIOUS_APP_VERSION))
+    }
+
+    @Test
+    fun getEventAttributes_returnHashMapWithEmptyMapWhenStatusIsNoChange() {
+        val helper = BlueshiftInstallationStatusHelper()
+        val attributes = helper.getEventAttributes(BlueshiftInstallationStatus.NO_CHANGE, null)
+        assert(attributes.isEmpty())
     }
 }
